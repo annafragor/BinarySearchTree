c6cced48b25a7284cdfb59a365ed5466c7738213
moskanka <moskanka@yandex.ru>
2016-09-22 01:31:07 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,6 +3,16 @@
 #include <fstream>
 #include "../include/BinarySearchTree.h"
 
+template <typename T>
+void print(std::ostream& out, BinarySearchTree<T>& tree, std::string name){
+    out << name << ": ";
+    if (tree.empty())
+        out << "is empty.\n";
+    else
+        out << tree << "\n";
+    return;
+}
+
 int main() {
     auto a = {4, 2, 3, 5, 1}; //a - std::initializer_list<int>]
     BinarySearchTree<int> b(a);
@@ -12,68 +22,63 @@ int main() {
     if(b.find(8))
         std::cout << "such value as 8 was found." << std::endl; //such value as 8 wasn't found.
 
-    std::cout << b << std::endl;
-
+    print(std::cout, b, "b");
     if (b.insert(4))
         std::cout << "4 was inserted" << std::endl; //не выводит, тк элемент 4 уже есть
     if (b.insert(10))
         std::cout << "10 was inserted" << std::endl;
-
-
-    if (b.empty())
-        std::cout << "tree is empty.";
-    else
-        std::cout << b << std::endl;
+    print(std::cout, b, "b");
 
     BinarySearchTree<int> bin;
+    std::cout << "input bin: ";
     std::cin >> bin; //проверка потокового ввода
-
-    std::cout << "bin: " << bin << std::endl;
-
+    
+    print(std::cout, bin, "bin");
+    
     std::fstream f("D://!BMSTU//Programming//3semester//BinarySearchTree//examples//file.txt", std::ios::out); //открываем и очищаем файл
     if (!f.is_open())
     { std::cout << "can't open the file" << std::endl; return 0; }
-    if (bin.empty())
-        f << "tree is empty.";
-    else
-        f << bin; //прoверка файлового вывода
+    print(f, bin, "");
 
     BinarySearchTree<int> bout;
     f.close();
     f.open("D://!BMSTU//Programming//3semester//BinarySearchTree//examples//file2.txt", std::ios::in);
     f.seekg(0, std::ios::beg);
     f >> bout; //прoверка файлового ввода
-    std::cout << "read from file:" << std::endl; // bout; //просмотр того, что считалось из файла
-    if (bout.empty())
-        std::cout << "tree is empty.";
-    else
-        std::cout << bout;
-
-    std::cout << "\n\n!\n\n";
+    std::cout << "read from file: "; // bout; //просмотр того, что считалось из файла
+    print(std::cout, bout, "bout");
 
     auto list = {1, 2, 3, 4, 5};
-    BinarySearchTree<int> b1(list);
-    std::cout << "____________\n";
-    BinarySearchTree<int> b2(std::move(b1)); //конструктор перемещения
-    std::cout << std::endl << "b2:" << std::endl;
-    if (b2.empty())
-        std::cout << "b2 is empty." << std::endl;
-    else
-        std::cout << b2 << std::endl;
-
-    std::cout << std::endl << "b1:" << std::endl;
-    if (b1.empty())
-        std::cout << "b1 is empty." << std::endl; //b1 is empty
-    else
-        std::cout << b1 << std::endl;
-
     auto newList = {9, 8, 7, 6, 5};
+    BinarySearchTree<int> b1(list);
+    print(std::cout, b1, "b1 before moving");
+    BinarySearchTree<int> b2(std::move(b1)); //конструктор перемещения
+    print(std::cout, b2, "b2 after move c-tor");
+    print(std::cout, b1, "b1 after moving");
+
     BinarySearchTree<int> bst(newList);
+    print(std::cout, bst, "bst");
     b2 = std::move(bst); //move operator=
-    std::cout << std::endl << "b2 after operator=" << std::endl;
-    std::cout << b2;
-    if (bst.empty())
-        std::cout << "\nbst is empty." << std::endl; //bst is empty
+    print(std::cout, bst, "bst after moving to b2 by move=");
+    print(std::cout, b2, "b2");
+
+    BinarySearchTree<int> c(newList);
+    print(std::cout, c, "c");
+    BinarySearchTree<int> newC(a);
+    print(std::cout, newC, "newC");
+    newC = c;
+    print(std::cout, newC, "newC (after copy= of c)");
+
+    BinarySearchTree<int> newnewC(newC);
+    print(std::cout, newnewC, "newnewC (after move c-tor)");
+
+    if (c == newnewC)
+        std::cout << "c == newnewC" << std::endl;
     else
-        std::cout << bst << std::endl;
+        std::cout << "c != newnewC" << std::endl;
+
+    if (b == c)
+        std::cout << "b == c" << std::endl;
+    else
+        std::cout << "b != c" << std::endl;
 }
