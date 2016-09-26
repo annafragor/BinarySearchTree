96ff84a1569710882182d247507fa9df00047f68
moskanka <moskanka@yandex.ru>
2016-09-26 23:07:34 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -11,6 +11,17 @@ void print(std::ostream& out, BinarySearchTree<T>& tree, std::string name)
     return;
 }
 
+template <typename T>
+void print(std::ofstream& out, BinarySearchTree<T>& tree, std::string name)
+{
+    out << name << ": ";
+    if (tree.empty())
+        out << "is empty.\n";
+    else
+        out << tree << "\n";
+    return;
+}
+
 int main()
 {
     auto a = {4, 2, 3, 5, 1}; //a - std::initializer_list<int>
@@ -22,8 +33,8 @@ int main()
         std::cout << "such value as 5 was found." << std::endl;
     if(b.find(8))
         std::cout << "such value as 8 was found." << std::endl; //such value as 8 wasn't found.
-
     print(std::cout, b, "b");
+
     if (b.insert(4))
         std::cout << "4 was inserted" << std::endl; //не выводит, тк элемент 4 уже есть
     if (b.insert(10))
@@ -36,16 +47,15 @@ int main()
 
     print(std::cout, bin, "bin");
 
-    std::fstream f("D://!BMSTU//Programming//3semester//BinarySearchTree//examples//file.txt", std::ios::out); //открываем и очищаем файл
-    if (!f.is_open())
+    std::ofstream fout("D://!BMSTU//Programming//3semester//BinarySearchTree//examples//file.txt", std::ios::out); //открываем и очищаем файл
+    if (!fout.is_open())
     { std::cout << "can't open the file" << std::endl; return 0; }
-    print(f, bin, "");
+    print(fout, bin, "bin");
 
     BinarySearchTree<int> bout;
-    f.close();
-    f.open("D://!BMSTU//Programming//3semester//BinarySearchTree//examples//file2.txt", std::ios::in);
-    f.seekg(0, std::ios::beg);
-    f >> bout; //прoверка файлового ввода
+    std::ifstream fin("D://!BMSTU//Programming//3semester//BinarySearchTree//examples//file2.txt", std::ios::in);
+    fin.seekg(0, std::ios::beg);
+    fin >> bout; //прoверка файлового ввода
     std::cout << "read from file: "; // bout; //просмотр того, что считалось из файла
     print(std::cout, bout, "bout");
 
